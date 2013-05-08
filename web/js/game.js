e014fe997f2e0b7387313b9ad6154a04f03373ff
Yoni Pomper <yonipomper@gmail.com>
2013-05-08 00:28:32 -0400

    no change--just the c/c of justins snippet

--- web/js/game.js
+++ web/js/game.js
@@ -7,36 +7,36 @@
  *                            adds all the cards of the current client player
  */
 $(document).ready($.ajax({
-    url: 'InitializeGame',
-    type: 'POST',
-    dataType: 'json',
-    success: function(data) {
-        $.each(data.players, function(index, player) {
-            var jsonPlayer = $.parseJSON(player);
-            $("#players")
-                .append($('<div/>')
-                    .css("float", "left")
-                    .append($('<img />')
-                        .attr('src', jsonPlayer.image)
-                        .attr('height', jsonPlayer.height)
-                        .attr('width', jsonPlayer.width))
-                    .append($('<p />').text(jsonPlayer.name)));
-        });
+   url: 'InitializeGame',
+   type: 'POST',
+   dataType: 'json',
+   success: function(data) {
+       $.each(data.players, function(index, player) {
+           var jsonPlayer = $.parseJSON(player);
+           $("#players")
+               .append($('<div/>')
+                   .css("float", "left")
+                   .append($('<img />')
+                       .attr('src', jsonPlayer.image)
+                       .attr('height', jsonPlayer.height)
+                       .attr('width', jsonPlayer.width))
+                   .append($('<p />').text(jsonPlayer.name)));
+       });
 
-        $.each(data.cards, function(index, card) {
-            var jsonCard = $.parseJSON(card);
-            $("#cards")
-                .append($('<div/>')
-                    .css("float", "left")
-                    .append($('<img />')
-                        .attr('src' ,jsonCard.image)
-                        .attr('height', jsonCard.height)
-                        .attr('width', jsonCard.width)));
-        });
-    },
-    error: function() {
-        alert('failure');
-    }}));
+       $.each(data.cards, function(index, card) {
+           var jsonCard = $.parseJSON(card);
+           $("#cards")
+               .append($('<div/>')
+                   .css("float", "left")
+                   .append($('<img />')
+                       .attr('src' ,jsonCard.image)
+                       .attr('height', jsonCard.height)
+                       .attr('width', jsonCard.width)));
+       });
+   },
+   error: function() {
+       alert('failure');
+   }}));
 
 /*
  * Polls for updates to the game
